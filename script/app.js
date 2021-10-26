21334e0ea4bf33e7ba20c2dbef8d09333ab40ec4
Simon <simon.coudeville@howest.be>
2021-10-26 09:01:55 +0200

    Updates

--- script/app.js
+++ script/app.js
@@ -11,7 +11,6 @@ let contentEl = document.querySelector('.js-content');
 let previousButtonEl = document.querySelector('.js-previous-day');
 let nextButtonEl = document.querySelector('.js-next-day');
 let todayButtonEl = document.querySelector('.js-today');
-let bottomEl = document.querySelector('.js-bottom');
 let picture = new Image();
 
 const today = new Date();
@@ -43,7 +42,6 @@ function setPictureDate() {
 
 setPictureDate();
 
-// Defining async function
 async function getapi(url, date) {
   // Storing response
   const response = await fetch(
@@ -61,10 +59,8 @@ async function getapi(url, date) {
   }
 }
 
-// Calling that async function
 getapi(api_url, pictureDate);
 
-// Function to hide the loader
 function hideloader(callback) {
   appEl.classList.remove('is-starting', 'is-loading');
   callback();
@@ -76,39 +72,33 @@ function showloader() {
 
 function checkToday() {
   if (pictureDate === todayDate) {
-    nextButtonEl.style.display = 'none';
-    todayButtonEl.style.display = 'none';
+    appEl.classList.add('is-today');
+    appEl.classList.remove('is-yesterday');
   } else if (pictureDate === yesterdayDate) {
-    nextButtonEl.style.display = 'flex';
-    todayButtonEl.style.display = 'none';
+    appEl.classList.add('is-yesterday');
+    appEl.classList.remove('is-today');
   } else {
-    nextButtonEl.style.display = 'flex';
-    todayButtonEl.style.display = 'flex';
+    appEl.classList.remove('is-yesterday', 'is-today');
   }
 }
 
-function setDocumentHeight() {
+function scrollImage() {
   const viewportHeight = window.innerHeight;
   let bodyHeight = document.body.clientHeight;
-  let checkpoint = (bodyHeight - viewportHeight) * 1.25;
+  let scrollDistance = bodyHeight - viewportHeight;
 
   window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
-    if (currentScroll <= checkpoint) {
-      opacity = 1 - currentScroll / checkpoint;
-    } else {
-      opacity = 0;
-    }
-
-    pictureEl.style.opacity = opacity;
+    const scrollDifference = currentScroll / scrollDistance;
+    pictureEl.style.setProperty('--scrollDifference', scrollDifference);
   });
 }
 
-function show(data, callback) {
+function show(data) {
   checkToday();
 
   if (data.media_type === 'video') {
-    hideloader(setDocumentHeight);
+    hideloader(scrollImage);
     pictureEl.innerHTML = `
     <div class="app__video">
       <iframe class="app__player" src="${data.url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -116,9 +106,8 @@ function show(data, callback) {
     `;
   } else {
     picture.src = data.url;
-
     picture.onload = function () {
-      hideloader(setDocumentHeight);
+      hideloader(scrollImage);
     }
     pictureEl.innerHTML = `
       <img class="app__img" src="${data.url}" alt="${data.title}">
